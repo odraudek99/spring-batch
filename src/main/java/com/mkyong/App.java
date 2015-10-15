b588f81f4a9c60e7db8e8b91c6ed82697be8f28a
eduardo <Krayola91>
2015-10-14 19:25:58 -0500

    first commit

---
+++ src/main/java/com/mkyong/App.java
@@ -0,0 +1,45 @@
+package com.mkyong;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	
+//	public static final String LOG_CONFIG = "/SIAT_NFS/pdc/configuracion/log4j.properties";
+//	
+//	static {
+//	        PropertyConfigurator
+//	                .configure("/SIAT_NFS/pdc/configuracion/log4j.properties");
+//	    }
+	
+	public static void main(String[] args) {
+
+		String[] springConfig  = 
+			{	
+				//"classpath:spring/batch/config/context.xml",
+				"classpath:spring/baseContext.xml" 
+			};
+		
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext(springConfig);
+		
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("reportJob");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Done");
+
+	}
+}
